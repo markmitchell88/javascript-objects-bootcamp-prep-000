84c9609b24593949cee4bf767965bf5c5409bded
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-02-19 21:28:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(object,artistName,songTitle){
   playlist[artistName]=songTitle;
 return playlist
 }
+
+function removeFromPlaylist(object,artistName){
+  delete playlist.artistName
+  return playlist
+}
