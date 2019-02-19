50d1757db222f92c4d9b8e7ce80584eeadb59007
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-02-19 21:17:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { Nirvana: 'lithium'}
 
-function updatePlaylist(artistName,songTitle){
-  return playlist.assign(artistName,songTitle)
+function updatePlaylist(playlist,artistName,songTitle){
+  return playlist.assign({}, playlist, { [artistName]: songTitle })
 }
